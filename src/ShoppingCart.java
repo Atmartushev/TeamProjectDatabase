24074504644c013954ae3807d03df9cc64e56034
Atmartushev <atmartushev@gmail.com>
2023-12-05 15:09:08 -0600

    Initialize repo

---
+++ src/ShoppingCart.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author avtonommartushev
+ */
+public class ShoppingCart {
+    
+    private int shoppingCartID;
+    
+    public ShoppingCart()
+    {
+        
+    }
+
+    public int getShoppingCartID() {
+        return shoppingCartID;
+    }
+    
+    
+    
+    
+}
